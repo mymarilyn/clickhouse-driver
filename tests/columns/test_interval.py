62dc6af75f7cf8b47e708d32ace18b1dd02f6db5
Krisztián Szűcs <szucs.krisztian@gmail.com>
2018-01-09 09:40:41 +0100

    fix interval test

--- tests/columns/test_interval.py
+++ tests/columns/test_interval.py
@@ -4,28 +4,21 @@ from tests.testcase import BaseTestCase
 class IntervalTestCase(BaseTestCase):
 
     def test_all(self):
-        columns = (
-            'a IntervalDay, b IntervalWeek, c IntervalMonth, d IntervalYear, '
-            'e IntervalHour, f IntervalMinute, g IntervalSecond'
-        )
+        interval = {
+            'YEAR': 1,
+            'MONTH': 2,
+            'WEEK': 3,
+            'DAY': 4,
+            'HOUR': 5,
+            'MINUTE': 6,
+            'SECOND': 7
+        }
+        columns = ', '.join(['INTERVAL {} {}'.format(v, k)
+                             for k, v in interval.items()])
+        query = 'SELECT {} FROM system.numbers'.format(columns)
 
-        data = [
-            (-10, -300, -123581321, -123581321345589144,
-             10, 300, 123581321, 123581321345589144)
-        ]
-        with self.create_table(columns):
-            self.client.execute(
-                'INSERT INTO test (a, b, c, d, e, f, g, h) VALUES', data
-            )
+        cli_result = self.emit_cli(query)
+        self.assertEqual(cli_result, '1\t2\t3\t4\t5\t6\t7\n')
 
-            query = 'SELECT * FROM test'
-            inserted = self.emit_cli(query)
-            self.assertEqual(
-                inserted, (
-                    '-10\t-300\t-123581321\t-123581321345589144\t'
-                    '10\t300\t123581321\t123581321345589144\n'
-                )
-            )
-
-            inserted = self.client.execute(query)
-            self.assertEqual(inserted, data)
+        client_result = self.client.execute(query)
+        self.assertEqual(client_result, cli_result)
