2484951b60c772cc7856b88645dfd2cf30458a36
Grigory Tarasenko <fle4er@yandex.ru>
2019-04-01 09:20:30 +0700

    Change convertation to date

--- clickhouse_driver/columns/datecolumn.py
+++ clickhouse_driver/columns/datecolumn.py
@@ -12,8 +12,7 @@ class DateColumn(FormatColumn):
     epoch_end = date(2105, 1, 1)
 
     def before_write_item(self, value):
-        if not isinstance(value, date):
-            value = value.date()
+        value = date(value.year, value.month, value.day)
         diff = (value - self.epoch_start).days
         if value > self.epoch_end or diff < 0:
             return 0
