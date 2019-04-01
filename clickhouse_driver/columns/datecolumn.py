2a055b6e961991d08d409d0ba1fbbec252a55e8c
Grigory Tarasenko <fle4er@yandex.ru>
2019-04-01 16:29:38 +0700

    Fixed inserting datetime as date (#75)
    Fixed inserting datetime as date

--- clickhouse_driver/columns/datecolumn.py
+++ clickhouse_driver/columns/datecolumn.py
@@ -9,9 +9,14 @@ class DateColumn(FormatColumn):
     format = 'H'
 
     epoch_start = date(1970, 1, 1)
+    epoch_end = date(2105, 1, 1)
 
     def before_write_item(self, value):
-        return (value - self.epoch_start).days
+        value = date(value.year, value.month, value.day)
+        diff = (value - self.epoch_start).days
+        if value > self.epoch_end or diff < 0:
+            return 0
+        return diff
 
     def after_read_item(self, value):
         return self.epoch_start + timedelta(value)
--- tests/columns/test_date.py
+++ tests/columns/test_date.py
@@ -1,5 +1,5 @@
 import os
-from datetime import date
+from datetime import date, datetime
 
 from freezegun import freeze_time
 from mock import patch
@@ -23,3 +23,24 @@ class DateTestCase(BaseTestCase):
             with patch.dict(os.environ, {'TZ': 'US/Hawaii'}):
                 inserted = self.client.execute(query)
                 self.assertEqual(inserted, data)
+
+    def test_insert_datetime_to_date(self):
+        with self.create_table('a Date'):
+            testTime = datetime(2015, 6, 6, 12, 30, 54)
+            self.client.execute(
+                'INSERT INTO test (a) VALUES', [(testTime, )]
+            )
+            query = 'SELECT * FROM test'
+            inserted = self.emit_cli(query)
+            self.assertEqual(inserted, '2015-06-06\n')
+
+    def test_wrong_datetime_insert(self):
+        with self.create_table('a Date'):
+            wrongTime = date(5555, 1, 1)
+            nullTime = date(1, 1, 1)
+            self.client.execute(
+                'INSERT INTO test (a) VALUES', [(wrongTime, ), (nullTime, )]
+            )
+            query = 'SELECT * FROM test'
+            inserted = self.emit_cli(query)
+            self.assertEqual(inserted, '0000-00-00\n0000-00-00\n')
