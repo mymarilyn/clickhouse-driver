5803ba583ff3683341ebc51a1fd9447bef42d676
Konstantin Lebedev <kostyan.lebedev@gmail.com>
2021-09-19 18:38:21 +0300

    Fix numpy bytes order

--- clickhouse_driver/columns/numpy/base.py
+++ clickhouse_driver/columns/numpy/base.py
@@ -8,7 +8,7 @@ class NumpyColumn(Column):
 
     def read_items(self, n_items, buf):
         data = buf.read(n_items * self.dtype.itemsize)
-        return np.frombuffer(data, self.dtype, n_items)
+        return np.frombuffer(data, self.dtype.newbyteorder('<'), n_items)
 
     def write_items(self, items, buf):
-        buf.write(items.astype(self.dtype).tobytes())
+        buf.write(items.astype(self.dtype.newbyteorder('<')).tobytes())
