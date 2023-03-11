d7844aff3eab7c8a13589249b4d740231e0c6ac0
Vitalik <56512948+notsovitalik@users.noreply.github.com>
2023-03-12 00:52:09 +0300

    add possibility to don't replace non-words by default (#361)
    Add possibility to don't replace non-words by default

---
+++ tests/columns/test_columns_names.py
@@ -0,0 +1,17 @@
+from tests.testcase import BaseTestCase
+
+class ColumnsNamesTestCase(BaseTestCase):
+
+    def test_columns_names_replace_nonwords(self):
+        columns = (
+            'regular Int64, '
+            'CamelCase Int64, '
+            'With_Underscores Int64, '
+            'Any%different.Column? Int64'
+        )
+
+        expected_columns = ['regular', 'CamelCase', 'With_Underscores', 'Any%different.Column?']
+
+        with self.create_table(columns):
+            df = self.client.query_dataframe('SELECT * FROM test', replace_nonwords=False)
+            self.assertTrue(expected_columns.equals(list(df.columns)))
