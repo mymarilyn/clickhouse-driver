a1eeedcdf3e3b59ffed0f530b3ca3ccf11c546a2
insomnes <denginm@gmail.com>
2023-09-26 12:19:12 +0200

    Remove send_receive_timeot setting, add test and settings explaning comment

--- tests/test_long_insert.py
+++ tests/test_long_insert.py
@@ -8,10 +8,24 @@ class LongInsertTestCase(BaseTestCase):
             'send_timeout': 1,
             'receive_timeout': 1,
         },
-        'send_receive_timeout': 2,
     }
 
     def test_long_insert(self):
+        """
+        In this test we are trying to emulate the situation, where we have a
+        lot of insert blocks. From specific clickhouse version server would
+        send ProfileEvents packet in response to each insert.
+
+        This insert should work normally for all clickhouse versions,
+        even without response ProfileEvents on each insert.
+        The 100_000 rows used to provide somewaht consistent experience of
+        bug reproducability without too long test duration.
+
+        `send_timeout` & `receive_timeout` are set to 1,
+        so we can emulate the real world situation on synthetic data.
+        The server will send exception and timeout if the client will not
+        receive the ProfileEvent during this time.
+        """
         with self.create_table('x Int32'):
             data = [{'x': 1}] * 100_000
             self.client.execute(
