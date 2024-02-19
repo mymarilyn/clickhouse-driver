c183ae12b5e38c0d37a253248e94867d4b93e4f1
Konstantin Lebedev <kostyan.lebedev@gmail.com>
2024-02-19 19:29:17 +0300

    Test against latest 24.1.5.6

--- tests/test_query_info.py
+++ tests/test_query_info.py
@@ -78,7 +78,7 @@ class QueryInfoTestCase(BaseTestCase):
         self.assertEqual(last_query.elapsed, 0)
 
     def test_last_query_progress_total_rows(self):
-        self.client.execute('SELECT max(number) FROM numbers(10)')
+        self.client.execute('SELECT number FROM numbers(10) LIMIT 10')
 
         last_query = self.client.last_query
         self.assertIsNotNone(last_query)
@@ -144,8 +144,8 @@ class QueryInfoTestCase(BaseTestCase):
         last_query = self.client.last_query
         self.assertIsNotNone(last_query)
         self.assertIsNotNone(last_query.progress)
-        self.assertGreater(last_query.progress.rows, 100000000)
-        self.assertGreater(last_query.progress.bytes, 800000000)
+        self.assertGreaterEqual(last_query.progress.rows, 100000000)
+        self.assertGreaterEqual(last_query.progress.bytes, 800000000)
 
         total_rows = 100000000 if self.server_version > (19, 4) else 0
         self.assertEqual(last_query.progress.total_rows, total_rows)
