b57c5694789d0e128991b5377b9dada9ccafd788
Konstantin Lebedev <kostyan.lebedev@gmail.com>
2024-08-08 10:38:55 +0300

    Add python 3.12 to setup.py

--- setup.py
+++ setup.py
@@ -102,6 +102,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: Implementation :: PyPy',
 
         'Topic :: Database',
